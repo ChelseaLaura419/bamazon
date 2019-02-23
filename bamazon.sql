e36c2d3af50475f8d36f73d87e9c1f44186b3be5
Chelsea Laura <cdesrosiers419@gmail.com>
2019-02-22 20:33:54 -0500

    Products added to SQL

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,15 @@ CREATE TABLE products(
   stock_quantity INT default 0,
   PRIMARY KEY (id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES  ('Shampoo', 'Cosmetics', 5.75, 500),
+		    ('Conditioner', 'Cosmetics', 6.25, 627),
+		    ('Trash Bags', 'Grocery', 5.99, 300),
+		    ('Paper Towels', 'Grocery', 4.25, 400),
+		    ('Zucchini', 'Produce', 0.85, 800),
+		    ('Avacado', 'Produce', 1.20, 1000),
+		    ('Chicken Breast', 'Meat', 4.45, 267),
+		    ('Cream Cheese', 'Dairy', 1.75, 476),
+		    ('Milk', 'Dairy', 2.50, 423),
+		    ('Chocolate Cake', 'Bakery', 12.75, 50)
